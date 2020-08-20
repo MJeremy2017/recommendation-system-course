2c274e8f58c661758885f5cc3cc24b2114f139ee
jeremy.zhang <zhangyue9306@gmail.com>
2020-08-20 10:26:47 +0800

    chall rbm

---
+++ Challenges/RBMBakeOff.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  3 11:11:13 2018
+
+@author: Frank
+"""
+
+from Challenges.MovieLens import MovieLens
+from Challenges.RBMAlgorithm import RBMAlgorithm
+from surprise import NormalPredictor
+from Challenges.Evaluator import Evaluator
+
+import random
+import numpy as np
+
+
+def LoadMovieLensData():
+    ml = MovieLens()
+    print("Loading movie ratings...")
+    data = ml.loadMovieLensLatestSmall()
+    print("\nComputing movie popularity ranks so we can measure novelty later...")
+    rankings = ml.getPopularityRanks()
+    return (ml, data, rankings)
+
+
+np.random.seed(0)
+random.seed(0)
+
+# Load up common data set for the recommender algorithms
+(ml, evaluationData, rankings) = LoadMovieLensData()
+
+# Construct an Evaluator to, you know, evaluate them
+evaluator = Evaluator(evaluationData, rankings)
+
+# RBM
+RBM = RBMAlgorithm(epochs=20)
+evaluator.AddAlgorithm(RBM, "RBM")
+
+# Just make random recommendations
+Random = NormalPredictor()
+evaluator.AddAlgorithm(Random, "Random")
+
+# Fight!
+evaluator.Evaluate(False)
+
